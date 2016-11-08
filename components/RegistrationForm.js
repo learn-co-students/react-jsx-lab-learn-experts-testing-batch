8c98636d02f16f130cf7534f2a317dd2cc1caf31
kwebster2 <kevin.r.webster@gmail.com>
2016-11-08 13:03:44 +0530

    test

---
+++ components/RegistrationForm.js
@@ -0,0 +1,11 @@
+const React = require('react')
+
+class RegistrationForm extends React.Component {
+  render(){
+    <form>
+      <input type="text"/>
+    </form>
+  }
+}
+
+module.exports = RegistrationForm
---
+++ components/Webpage.js
@@ -0,0 +1,9 @@
+const React = require('react')
+
+class Webpage extends React.Component {
+  render(){
+    <h1>The world's coolest webpage</h1>
+  }
+}
+
+module.exports = Webpage
